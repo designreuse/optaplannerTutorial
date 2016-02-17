4656d40f5b163e00768232b5543ae2a1b6cb2aaf
kpeet <kpeet@inf.utfsm.cl>
2016-02-17 14:45:54 -0300

    Problems Fact.
    Definición de las clases Problems Fact. Estas son las clases que no
    cambian sus valores a medida que se desarrolla el solver.
    
    Clases implementadas:
    
    Vehicle.class
    Depot.class
    
    interface implementada que conecta las cadenas.
    
    StandStill

---
+++ base project/src/main/java/cl/citymovil/optaplanner/domain/Standstill.java
@@ -0,0 +1,18 @@
+package cl.citymovil.optaplanner.domain;
+
+import org.optaplanner.core.api.domain.entity.PlanningEntity;
+import org.optaplanner.core.api.domain.variable.InverseRelationShadowVariable;
+
+
+@PlanningEntity
+public abstract interface Standstill {
+	public abstract Location getLocation();
+
+	public abstract Vehicle getVehicle();
+
+	@InverseRelationShadowVariable(sourceVariableName = "previousStandstill")
+	public abstract Customer getNextCustomer();
+
+	public abstract void setNextCustomer(Customer paramCustomer);
+
+}
